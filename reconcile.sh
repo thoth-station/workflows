736a6b94df6a7313b701c1296cac8cb41ad1a95e
Christoph Görn <goern@redhat.com>
2019-11-07 13:20:30 +0100

    a workflow reconciliation script
    Signed-off-by: Christoph Görn <goern@redhat.com>

---
+++ reconcile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+oc delete workflow $(oc get workflows -o=jsonpath='{.items[?(@.status.phase=="Succeeded")].metadata.name}')
+
+for w in $( oc get workflows -o=jsonpath='{.items[?(@.status.phase=="Error")].metadata.name}' ); do
+    argo retry $w
+done
+
+#end.
