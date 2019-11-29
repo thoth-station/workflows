5962c8696766188f0fe2d6bf63220f6842aef7a2
Christoph Görn <goern@redhat.com>
2019-11-29 08:39:48 +0100

    more elaborated trivial reconcile loop for solver workflows
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- reconcile.sh
+++ reconcile.sh
@@ -2,9 +2,15 @@
 
 oc delete workflow $(oc get workflows -o=jsonpath='{.items[?(@.status.phase=="Succeeded")].metadata.name}')
 
-for w in $( oc get workflows -o=jsonpath='{.items[?(@.status.phase=="Error")].metadata.name}' ); do
-    argo retry $w
-    sleep 7
+while :
+do
+    for w in $(oc get workflows -o=jsonpath='{.items[?(@.status.phase=="Error")].metadata.name}' ); do
+        argo retry $w
+        sleep 7
+        oc delete workflow $(oc get workflows -o=jsonpath='{.items[?(@.status.phase=="Succeeded")].metadata.name}')
+    done
+
+    sleep 15
 done
 
 #end.
